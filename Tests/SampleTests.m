75cf7d02c2c18a6a631268d3ebbfab6a75f9e71d
David Whitehurst <dlwhitehurst@me.com>
2011-07-12 18:48:54 -0400

    general progress

--- Tests/SampleTests.m
+++ Tests/SampleTests.m
@@ -1,9 +1,19 @@
 //
-//  SampleTests.m
-//  Machine
+// SampleTests.m
+// 
+// Created by David L. Whitehurst on 06/18/2011.
+// Copyright 2011. All rights reserved.
 //
-//  Created by David Whitehurst on 6/18/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+// This software and associated documentation is PROPRIETARY and 
+// PERMISSION TO USE "THE SOFTWARE" IS NOT allowed. In the event  
+// "THE SOFTWARE" becomes available to you without written consent
+// or transfer of its ownership it must be destroyed and cannot be
+// used without the explicit permission of the copyright holder
+// listed in the copyright notice in the top of this header.
+//
+// THE ABOVE COPYRIGHT AND PROPRIETARY NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF "THE SOFWARE".
+//
 //
 
 #import <GHUnitIOS/GHUnit.h>
