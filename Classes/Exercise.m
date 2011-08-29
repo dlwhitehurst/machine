09e90341ab4b2c6e16ba19ee06d8bd3277cff870
David Whitehurst <dlwhitehurst@me.com>
2011-08-29 00:05:48 -0400

    New headers for all model objects

--- Classes/Exercise.m
+++ Classes/Exercise.m
@@ -1,17 +1,21 @@
-/*********************************************************************
- File: Exercise.m
- 
- Abstract: This module represents the root object or parent to hold
- one-to-many exercise item objects.  This object is uniquely identified
- by the date or day.
- 
- Version: 1.0
- Author: David L. Whitehurst
- Created: March 13, 2011
- 
- Copyright (C) 2010 David L. Whitehurst. All Rights Reserved.
- 
- ********************************************************************/
+//
+// Exercise.m
+// 
+// Created by David L. Whitehurst on 03/13/2011.
+// Modified 08/28/2011
+// Copyright 2011. All rights reserved.
+//
+// This software and associated documentation is PROPRIETARY and 
+// PERMISSION TO USE "THE SOFTWARE" IS NOT allowed. In the event  
+// "THE SOFTWARE" becomes available to you without written consent
+// or transfer of its ownership it must be destroyed and cannot be
+// used without the explicit permission of the copyright holder
+// listed in the copyright notice in the top of this header.
+//
+// THE ABOVE COPYRIGHT AND PROPRIETARY NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF "THE SOFWARE".
+//
+//
 
 #import "Exercise.h"
 
